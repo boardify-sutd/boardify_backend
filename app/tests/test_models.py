6572bb432e6eed5f54660d324c1f534ab28b1c28
Lionell Loh Jian An <lionell_loh@mymail.sutd.edu.sg>
2019-05-20 01:14:51 +0800

    normalize email feature

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -17,3 +17,14 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+
+    def test_new_user_email_normalized(self):
+        """Test that the email for a new user is normalized (reduced to lower case)"""
+        email = "liOnelL@gmail.com"
+        user = get_user_model().objects.create_user(email, "test123")
+
+        self.assertEqual(user.email, email.lower())
+
+
+
