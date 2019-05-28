c4a9cfe3c8612086ba1f4e8b5972643296b7ea58
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2019-05-29 06:34:43 +0800

    create module API

--- app/boards/views.py
+++ app/boards/views.py
@@ -7,10 +7,20 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Module
 from boards import serializers
 
-class ModuleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class ModuleViewSet(viewsets.GenericViewSet,
+                    mixins.ListModelMixin,
+                    mixins.CreateModelMixin):
     """Manage Modules in the database"""
 
     authentication_classes = (TokenAuthentication, )
     permission_classes = (IsAuthenticated, )
     queryset = Module.objects.all()
     serializer_class = serializers.ModuleSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """Create a new tag"""
+        serializer.save(user=self.request.user)
