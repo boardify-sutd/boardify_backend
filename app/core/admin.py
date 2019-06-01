5d27e2f45ac20565ef43f64209cf0f0cfd15708b
lionellloh <lionell_loh@mymail.sutd.edu.sg>
2019-06-02 01:09:17 +0800

    registered models on admin site for sanity check on admin GUI

--- app/core/admin.py
+++ app/core/admin.py
@@ -14,3 +14,20 @@ admin.site.register(models.Lesson)
 admin.site.register(models.Lecturer)
 admin.site.register(models.Board)
 admin.site.register(models.Location)
+admin.site.register(models.Caption)
+admin.site.register(models.Comment)
+admin.site.register(models.Pillar)
+admin.site.register(models.CohortClass)
+admin.site.register(models.LessonType)
+
+
+"""Create model Caption
+    - Create model LessonType
+    - Create model Pillar
+    - Remove field id from board
+    - Add field duration to lesson
+    - Alter field type on lesson
+    - Create model Comment
+    - Create model CohortClass
+    - Add field caption to board
+    - Add field class_group to lesson"""
